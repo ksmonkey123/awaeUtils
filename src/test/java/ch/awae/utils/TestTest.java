a88885ba90273cd46168065d5875a78241060247
Andreas Wälchli <andreas.waelchli@me.com>
2016-08-22 23:50:00 +0200

    commit

---
+++ src/test/java/ch/awae/utils/TestTest.java
@@ -0,0 +1,14 @@
+package ch.awae.utils;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestTest extends TestCase {
+
+	@Test
+	public void test() throws Exception {
+		assertEquals(4, ch.awae.utils.Test.getFour());
+	}
+	
+}
