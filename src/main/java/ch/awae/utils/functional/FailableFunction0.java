a831d74b1de6c42a27bbb8e799f45c26d0b9a9a9
Andreas Wälchli <andreas.waelchli@me.com>
2018-03-01 00:19:16 +0100

    added Callable<T> as parent of FailableFunction0<T>. This allows FailableFunctions to be used in ExecutorServices. Some trickery was necessary for handling exceptions.

--- src/main/java/ch/awae/utils/functional/FailableFunction0.java
+++ src/main/java/ch/awae/utils/functional/FailableFunction0.java
@@ -1,5 +1,7 @@
 package ch.awae.utils.functional;
 
+import java.util.concurrent.Callable;
+
 /**
  * Similar to {@link java.util.function.Supplier} but able to throw any
  * arbitrary exception.
@@ -8,8 +10,22 @@ package ch.awae.utils.functional;
  * @since awaeUtils 1.0.0
  */
 @FunctionalInterface
-public interface FailableFunction0<T> {
+public interface FailableFunction0<T> extends Callable<T> {
 
     T apply() throws Throwable;
+
+    @Override
+    default T call() throws Exception {
+        try {
+            return apply();
+        } catch (Throwable e) {
+            if (e instanceof Exception)
+                throw (Exception) e;
+            else if (e instanceof Error)
+                throw (Error) e;
+            else
+                throw new Exception(e);
+        }
+    }
 
 }
